f6358c899d27fb7b70b50ba0040d8ca17b6c205d
AninhaMel <ana.silva5134@etec.sp.gov.br>
2023-11-06 09:40:27 -0300

    Account controller

--- MtecDevs/Controllers/AccountController.cs
+++ MtecDevs/Controllers/AccountController.cs
@@ -1,25 +1,58 @@
+using System.Net.Mail;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MtecDevs.ViewModels;
 
 namespace MtecDevs.Controllers;
 
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+    
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(
+            ILogger<AccountController> logger, 
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl)
         {
-            return View();
+            LoginVM loginVM = new() {
+                UrlRetorno = returnUrl ?? Url.Content("~/")
+            };
+            return View(loginVM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Login(LoginVM login)
+        {
+            if (ModelState.IsValid)
+            {
+                //Verifico o login
+                
+            }
+            return View(login);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        private static bool IsValidEmail(string email)
         {
-            return View("Error!");
+            try
+            {
+                MailAddress m = new(email);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
+
