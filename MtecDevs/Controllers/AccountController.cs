f8388531ad5de8d760e64dcc286cff1dbaa5c955
AninhaMel <ana.silva5134@etec.sp.gov.br>
2023-11-27 09:41:08 -0300

    Feito o register

--- MtecDevs/Controllers/AccountController.cs
+++ MtecDevs/Controllers/AccountController.cs
@@ -6,86 +6,124 @@ using MtecDevs.ViewModels;
 
 namespace MtecDevs.Controllers;
 
-    public class AccountController : Controller
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(ILogger<AccountController> logger,
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager)
     {
-        private readonly ILogger<AccountController> _logger;
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
-    
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
 
-        public AccountController(
-            ILogger<AccountController> logger, 
-            SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager)
+    [HttpGet]
+    public IActionResult Login(string ReturnUrl)
+    {
+        LoginVM login = new()
         {
-            _logger = logger;
-            _signInManager = signInManager;
-            _userManager = userManager;
-        }
+            UrlRetorno = ReturnUrl ?? Url.Content("~/")
+        };
+        return View(login);
+    }
 
-        [HttpGet]
-        public IActionResult Login(string returnUrl)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
         {
-            LoginVM loginVM = new() {
-                UrlRetorno = returnUrl ?? Url.Content("~/")
-            };
-            return View(loginVM);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult>Login(LoginVM login)
-        {
-            if (ModelState.IsValid)
+            // Verifica o Login
+            string userName = login.Email;
+            // Verificando se o login é por email
+            if (IsValidEmail(login.Email))
             {
-                //Verifico o login
-                string userName = login.Email;
-                if (IsValidEmail(login.Email))
-                {
-                    var user = await _userManager.FindByEmailAsync(login.Email);
-                    if(user != null)
-                    {
-                        userName = user.UserName;
-                    }
-                }
-                var result = await _signInManager.PasswordSignInAsync(
-                    userName, login.Senha, login.Lembrar, lockoutOnFailure: true
-                );
-
-                if(result.Succeeded)
-                {
-                    _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
-                    return LocalRedirect(login.UrlRetorno);
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning($"Usuário {login.Email} foi bloqueado");
-                    return RedirectToAction("Lockout");
-                }
-
-                ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!");
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
             }
-            return View(login);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Logout()
-        {
-            _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
-        }
+            // Login é só por UserName
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
 
-        private static bool IsValidEmail(string email)
-        {
-            try
+            if (result.Succeeded)
             {
-                MailAddress m = new(email);
-                return true;
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
             }
-            catch
+            if (result.IsLockedOut)
             {
-                return false;
+                _logger.LogInformation($"Usuário {login.Email} está bloqueado");
+                return RedirectToAction("Lockout");
             }
+            ModelState.AddModelError(string.Empty, "Usário e/ou Senha inválida!");
+        }
+        return View(login);
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+    private static bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
         }
     }
 
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterVM model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.Nome,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Senha);
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {model.Email} registrado com sucesso.");
+
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        return View(model);
+    }
+
+}
